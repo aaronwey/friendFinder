25b72fe7fdcaa43a2be505ba20b47c723a33f72c
aaronwey <aaronwey@gmail.com>
2017-10-22 18:46:22 -0600

    2nd commit

---
+++ data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+		name: "Proto-Clown",
+		photo: "https://vignette3.wikia.nocookie.net/the-tick-animated/images/0/03/Protoclown.png/revision/latest?cb=20170414201001",
+		scores: [
+			3,
+			5,
+			1,
+			2,
+			4,
+			3,
+			5,
+			4,
+			1,
+			4
+		]
+	}
+];
+
+module.exports = friendsArray;
