92a3902707d4e7cd2d683af0480994965f553666
aaronwey <aaronwey@gmail.com>
2017-11-14 16:59:01 -0700

    3rd commit

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendData = [
+	{
+		name: "Proto-Clown",
+		photo: "<a href = 'https://vignette3.wikia.nocookie.net/the-tick-animated/images/0/03/Protoclown.png/revision/latest?cb=20170414201001' target = '_blank'><img src = 'https://vignette3.wikia.nocookie.net/the-tick-animated/images/0/03/Protoclown.png/revision/latest?cb=20170414201001'></a>",
+		scores: [4,4,4,4,4,4,4,4,4,4]
+	},
+		{
+		name: "Slime Princess",
+		photo: "<a href = 'https://vignette.wikia.nocookie.net/atfanfic/images/0/0f/Slime_Princess.png/revision/latest?cb=20140303164959' target = '_blank'><img src = 'https://vignette.wikia.nocookie.net/atfanfic/images/0/0f/Slime_Princess.png/revision/latest?cb=20140303164959'></a>",
+		scores: [5,5,5,5,5,5,5,5,5,5]
+	},
+];
+
+module.exports = friendData;
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var PORT = process.env.PORT || 8080;
+//Dependencies
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//Express set up
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+//Sets express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//Routes
+app.use(express.static('app'));
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(PORT, function(){
-	console.log("App listening on PORT: " + PORT);
+//Starts server
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
